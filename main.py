4a7487c783fa3885dbde3e084a21eb0f69ffd002
JCCC <juancruzcamayo893@gmail.com>
2026-08-23 13:11:51 -0300

    desarrollo de introduccion, bucle del programa, conexion a db por parametros personalizados y demostracion de contenidos de tablas en terminar con pandas

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-import mysql.connector as db, pandas as pd;
-print('\n\n-------------------------------\nCONSOLPY PROYECT\n-------------------------------\n\n\n')
+import mysql.connector as db, pandas as pd
+print('''  
+██████╗  ██████╗ ███╗   ██╗███████╗ ██████╗ ██╗  ██████╗██╗   ██╗
+██╔═══╝ ██╔═══██╗████╗  ██║██╔════╝██╔═══██╗██║  ██╔══██╗██╗ ██╔╝
+██║     ██║   ██║██╔██╗ ██║███████╗██║   ██║██║  ██████╔╝╚████╔╝
+██║     ██║   ██║██║╚██╗██║╚════██║██║   ██║██║  ██╔═══╝ ╔██╔╝
+╚██████╗╚██████╔╝██║ ╚████║███████║╚██████╔╝████╗██║    ╔██║
+ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═══╝╚═╝    ╚══╝''') # presentacion del proyecto
+print('inserte los datos que se le pidan para iniciar la conexion de la base de datos.')
+ejecucion=input('- ejcutar da database: ENTER.\n- salir del programa: 0\n\nEjecucion: ').strip()
+while ejecucion != '0':
+    database=input('-------------------------------\nnombre de la database a conectar.\n- Database: ').lower().strip()
+    while database == '':
+        print('-!-- el nombre de la database no puede estar vacio.\n')
+        database=input('nombre de la database a conectar.\n- Database: ').lower().strip()
+    user=input('usuario administradosr de la database.\n- Usuario: ').lower().strip()
+    password=input('contraseña de la dtabase. (si no tiene solo precione -ENTER-).\n- Contraseña: ').lower().strip()
+    port=input('introdusca el puerto en el que esta hosteada la database.\n- puerto: ').strip()
+    host=input('introdusca el hostname de origen de la database.\n- hostname: ').lower().strip() # inputs de conexion a la database
+    try:
+        conexion = db.connect( database= database,user= user or 'root',password= password or '',port=port or '3306',host=host or 'localhost') # conexion a la database
+        if conexion.is_connected():print(f'se conecto la base de datos"{database}"') # confirmacion de conexion a la database
+        else:print('no se conecto')
+        cursor = conexion.cursor()
+        table = input('----------------------------\ningrese nombre de tabla a mostrar.\n- Tabla: ').strip()
+        while table == '':
+            print('----------------------------\nel nombre de la tabla no puede estar vacio.')
+            table = input('----------------------------\ningrese nombre de tabla a mostrar.\n- Tabla: ').strip()
+        cursor.execute(f'SELECT * FROM {table}')
+        rows = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+        files = pd.DataFrame(rows,columns=columns)
+        print(files)
+        operacion = input('\n----------------------------\n- añadir nuevos productos a la tabla: N\n- borrar productos de la tabla: B\n- Modificar datos de los productos: M\n- cerrar conexion de la database: ENTER.\n- Cerrar programa: 0\n\nEjecucion: ').strip()
+    except Exception as error:print(f'\n----------------------------\nError: {error}.\n----------------------------\n')
+    ejecucion=input('\n\n- ejcutar da database: ENTER.\n- salir del programa: 0\n\nEjecucion: ').strip()
 
-print('inserte los datos que se le pidan para iniciar lla coneccion de la base de datos')
-database=input('nombre de la database a conectar.\n- Database: ').lower()
-user=input('usuario administradosr de la database.\n- Usuario: ').lower()
-password=input('contraseña de la dtabase. (si no tiene solo precione -ENTER-).\n- Contraseña: ').lower()
-port=input('introdusca el puerto en el que esta hosteada la database.\n- puerto: ')
-host=input('introdusca el hostname de origen de la database.\n- hostname: ')
-
-config_database = {
-    'port' : port,
-    'host' : host,
-    'user' : user,
-    'database' : database,
-    'password' : password
-}
-try:
-    conexion = db.connect(config_database)
-    if conexion.is_connected():print('se conecto la base de datos')
-    else:print('no se conecto')
-except:print('hubo algun error. mas que seguro un valor de conexion de la database invalido.')
